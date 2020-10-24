ae664b82b08ad00871f9f6c73478d27622358f9f
Denis Dementev <dementev629@gmail.com>
2020-10-24 22:32:39 +0300

    refactor project struct and add store interface

---
+++ backend/cmd/store/mongostorage/notification_repositort.go
@@ -0,0 +1,33 @@
+package mongostorage
+
+func test() {}
+
+// import (
+// 	"context"
+
+// 	"github.com/dementevda/likeisaid-gg/backend/cmd/api/models"
+// 	"go.mongodb.org/mongo-driver/bson"
+// 	"go.mongodb.org/mongo-driver/bson/primitive"
+// )
+
+// // AddUser adds user to database
+// func (s *MongoStorage) AddTask(u *models.User) (*models.User, error) {
+// 	u.Email = "qwe@qwe"
+// 	added, err := s.Db.Collection("users").InsertOne(context.TODO(), u)
+// 	u.ID = added.InsertedID.(primitive.ObjectID)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return u, nil
+
+// }
+
+// // FindUser search user in database
+// func (s *MongoStorage) ListALLTasks(u *models.User) (*models.User, error) {
+// 	user := &models.User{}
+// 	err := s.Db.Collection("users").FindOne(context.TODO(), bson.M{"email": u.Email}).Decode(user)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return user, err
+// }
--- backend/cmd/store/store.go
+++ backend/cmd/store/store.go
@@ -1,49 +1,11 @@
 package store
 
-import (
-	"context"
+import "github.com/dementevda/likeisaid-gg/backend/cmd/api/models"
 
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-)
-
-// MongoStorage for API
-type MongoStorage struct {
-	config *Config
-	client *mongo.Client
-	Db     *mongo.Database
-}
-
-//New ...
-func New(config *Config) *MongoStorage {
-	return &MongoStorage{
-		config: config,
-	}
-}
-
-// Open connection to storage
-func (s *MongoStorage) Open() error {
-	opts := options.Client().ApplyURI(s.config.DatabaseURL).SetAuth(options.Credential{AuthSource: s.config.DatabaseName, Username: s.config.DatabaseUser, Password: s.config.DatabasePasswd})
-	client, err := mongo.NewClient(opts)
-	if err != nil {
-		return err
-	}
-
-	if err := client.Connect(context.TODO()); err != nil {
-		return err
-	}
-
-	if err = client.Ping(context.TODO(), nil); err != nil {
-		return err
-	}
-
-	s.client = client
-	s.Db = client.Database(s.config.DatabaseName)
-
-	return nil
-}
-
-// Close connection to storage
-func (s *MongoStorage) Close() {
-	s.client.Disconnect(context.TODO())
+//Store interface for API storage
+type Store interface {
+	Open() error
+	Close()
+	AddUser(u *models.User) (*models.User, error)
+	FindUser(u *models.User) (*models.User, error)
 }
